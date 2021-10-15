d3390556230039a98e5610b79f6591015c690a0b
Facundo Vignoles <fvignoles@gmail.com>
2021-10-14 22:50:23 -0300

    modificaciones rutas y controllers

--- app.js
+++ app.js
@@ -11,32 +11,36 @@ app.use(express.static('public'));
 
 app.use(express.static('./src/views'));
 
+const mainRoutes = require('./src/routes/mainRoutes');
+
+app.use('/', mainRoutes);
+
 app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
 
-app.get('/login', (req, res) => { 
-    res.render("users/login.ejs") 
-});
+// app.get('/login', (req, res) => { 
+//      res.render("users/login.ejs") 
+// });
 
-app.get('/register', (req, res) => { 
-    res.render ("users/register.ejs") 
-});
+// app.get('/register', (req, res) => { 
+// //     res.render ("users/register.ejs") 
+// // });
 
-app.get('/detalle', (req, res) => { 
-    res.render("products/detalleproducto.ejs") 
-});
+// // app.get('/detalle', (req, res) => { 
+// //     res.render("products/detalleproducto.ejs") 
+// // });
 
-app.get('/carrito', (req, res) => { 
-    res.render("products/carrito.ejs") 
-});
+// // app.get('/carrito', (req, res) => { 
+// //     res.render("products/carrito.ejs") 
+// // });
 
-app.get('/', (req, res) => {
-     res.render("home/home.ejs") 
-});
+// // app.get('/', (req, res) => {
+// //      res.render("home/home.ejs") 
+// // });
 
-app.get('/agregarProducto',(req,res) =>{
-    res.render("products/agregarProducto.ejs");
-});
+// // app.get('/agregarProducto',(req,res) =>{
+// //     res.render("products/agregarProducto.ejs");
+// // });
 
-app.get('/editarProducto',(req,res) =>{
-    res.render("products/editarProducto.ejs");
-});
+// // app.get('/editarProducto',(req,res) =>{
+// //     res.render("products/editarProducto.ejs");
+// // });
