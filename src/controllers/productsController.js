02b9cef12e5559aee6dc79c0aa8c109401436a2f
agustincolman0310 <agustincolman0310@gmail.com>
2021-11-07 20:14:21 -0300

    subiendo cambios usuarios

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-
-const productsFilePath = path.join(
-  __dirname,
-  "../data/productsDataBase.json"
-);
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productsController = {
@@ -16,42 +12,39 @@ const productsController = {
     return res.render("products/carrito");
   },
 
-  	// Update - Form to edit
-	editar: (req, res) => {
-		let id = req.params.id
-		let productToEdit = products.find(
-			product => product.id == id
-		)
-		res.render('products/editarProducto', {productToEdit})
-	},
+  // Update - Form to edit
+  editar: (req, res) => {
+    let id = req.params.id;
+    let productToEdit = products.find((product) => product.id == id);
+    res.render("products/editarProducto", { productToEdit });
+  },
 
-	// Update - Method to update
-	update: (req, res) => {
-		let id = req.params.id;
-    let productoSeleccionado = products.find(product => product.id == id)
+  // Update - Method to update
+  update: (req, res) => {
+    let id = req.params.id;
+    let productoSeleccionado = products.find((product) => product.id == id);
 
-		let productToEdit = {
-			id: productoSeleccionado.id,
-		...req.body,
-	// 	name: productToEdit.name,
-  //   description: productToEdit.description,
-     image: productoSeleccionado.image, 
-  //  size: productToEdit.size,
-  //   dough: productToEdit.dough,
-  // price: productToEdit.price,
-	  };
-		
-		let newProducts = products.map(product => {
-			if (product.id == productoSeleccionado.id) {
-				return product = {...productToEdit};
-			}
-			return product;
-		})
+    let productToEdit = {
+      id: productoSeleccionado.id,
+      ...req.body,
+      // 	name: productToEdit.name,
+      //   description: productToEdit.description,
+      image: productoSeleccionado.image,
+      //  size: productToEdit.size,
+      //   dough: productToEdit.dough,
+      // price: productToEdit.price,
+    };
 
-		fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, ' '));
+    let newProducts = products.map((product) => {
+      if (product.id == productoSeleccionado.id) {
+        return (product = { ...productToEdit });
+      }
+      return product;
+    });
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, " "));
     products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-		res.redirect('/products/productos');
-
+    res.redirect("/products/productos");
   },
 
   agregar: (req, res) => {
@@ -62,39 +55,41 @@ const productsController = {
     res.render("products/productos", { products: products });
   },
 
-  	// Create -  Method to store
-	store: (req, res) => {
-		let newProduct = {
-			id: products[products.length - 1].id + 1,
-			...req.body};
+  // Create -  Method to store
+  store: (req, res) => {
+    let newProduct = {
+      id: products[products.length - 1].id + 1,
+      ...req.body,
+    };
 
-		  if (req.file == undefined) {
-        newProduct.image = "pizza1.jpg"}
-        else
-        {newProduct.image = req.file.filename }
-	
-		products.push(newProduct)
-		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
-    res.redirect('/products/productos');
-	},
+    if (req.file == undefined) {
+      newProduct.image = "pizza1.jpg";
+    } else {
+      newProduct.image = req.file.filename;
+    }
+
+    products.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+    res.redirect("/products/productos");
+  },
 
   detail: (req, res) => {
-    let id = req.params.id
-    let product = products.find(product => product.id == id)
-    res.render("products/detalleproducto", {product})
-},
+    let id = req.params.id;
+    let product = products.find((product) => product.id == id);
+    res.render("products/detalleproducto", { product });
+  },
 
-
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		let id = req.params.id;
-		let finalProducts = products.filter(product => product.id != id);
-		fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts, null, ' '));
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    let id = req.params.id;
+    let finalProducts = products.filter((product) => product.id != id);
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(finalProducts, null, " ")
+    );
     products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    res.redirect('/products/productos');
-	}
-
-
+    res.redirect("/products/productos");
+  },
 };
 
 module.exports = productsController;
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,19 +1,77 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, "../data/usersDataBase.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const usersController = {
-    login: (req,res) => {
-        return res.render('users/login');
-    },
-    register: (req,res) => {
-        return res.render('users/register');
-    },
-    list: (req,res) => {
-        res.render('users/userList',{'users': users});
-    },
-}
+  login: (req, res) => {
+    return res.render("users/login");
+  },
+  register: (req, res) => {
+    return res.render("users/register");
+  },
+  // Create -  Method to store
+  store: (req, res) => {
+    let newUser = {
+      id: users[users.length - 1].id + 1,
+      ...req.body,
+    };
+
+    if (req.file == undefined) {
+      newUser.image = "pizza1.jpg";
+    } else {
+      newUser.image = req.file.filename;
+    }
+
+    users.push(newUser);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+    res.redirect("/");
+  },
+  list: (req, res) => {
+    res.render("users/userList", { users: users });
+  },
+  // Update - Form to edit
+  editar: (req, res) => {
+    let id = req.params.id;
+    let userToEdit = users.find((user) => user.id == id);
+    res.render("users/editarUsuario", { userToEdit });
+  },
+
+  // Update - Method to update
+  update: (req, res) => {
+    let id = req.params.id;
+    let usuarioSeleccionado = users.find((user) => user.id == id);
+
+    let userToEdit = {
+      id: usuarioSeleccionado.id,
+      ...req.body,
+      image: usuarioSeleccionado.image,
+    };
+
+    let newUsers = users.map((user) => {
+      if (user.id == usuarioSeleccionado.id) {
+        return (user = { ...userToEdit });
+      }
+      return user;
+    });
+
+    fs.writeFileSync(usersFilePath, JSON.stringify(newUsers, null, " "));
+    users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+    res.redirect("users/userList");
+  },
+  detail: (req, res) => {
+    let id = req.params.id;
+    let user = users.find((user) => user.id == id);
+    res.render("users/detail", { user });
+  },
+  destroy: (req, res) => {
+    let id = req.params.id;
+    let finalUsers = users.filter((user) => user.id != id);
+    fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, " "));
+    users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+    res.redirect("users/userList");
+  },
+};
 
 module.exports = usersController;
