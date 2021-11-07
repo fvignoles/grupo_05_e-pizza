b956531486750b48111d3095c8a1c00682410543
Facundo Vignoles <fvignoles@gmail.com>
2021-11-07 20:49:29 -0300

    cambios img y json

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,18 +8,6 @@ const mainController = {
     home: (req,res) => {
         return res.render('home/home');
     },
-    // login: (req,res) => {
-    //     return res.render('users/login');
-    // },
-    // register: (req,res) => {
-    //     return res.render('users/register');
-    // },
-    // detalle: (req,res) => {
-    //     return res.render('products/detalleproducto');
-    // },
-    // carrito: (req,res) => {
-    //     return res.render('products/carrito');
-    // },
 }
 
 module.exports = mainController;
