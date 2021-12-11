2833fbeaa3f426382fe00d829b6e59d88c684f2d
agustincolman0310 <agustincolman0310@gmail.com>
2021-12-11 19:04:16 -0300

    subiendo cambios

--- Docs/databases/sql/data.sql
+++ Docs/databases/sql/data.sql
@@ -1,238 +1,235 @@
 USE `pizzadb`;
 
------------ Categories (users) -----------
+-- --------- Categories (users) -----------
 
-INSERT INTO pizzadb.categories
-(category_name, category_active)
-VALUES('admin', 1);
+INSERT INTO pizzadb.categories (category_name, category_active) VALUES('admin', 1); -- id 1
 
-INSERT INTO pizzadb.categories
-(category_name, category_active)
-VALUES('user', 1);
+INSERT INTO pizzadb.categories (category_name, category_active) VALUES('user', 1); -- id 2
 
-INSERT INTO pizzadb.categories
-(category_name, category_active)
-VALUES('guest', 1);
+INSERT INTO pizzadb.categories (category_name, category_active) VALUES('guest', 1); -- id 3
 
------------ Users -----------
+-- --------- Users -----------
 
 INSERT INTO pizzadb.users
 (user_category_id, user_firstname, user_lastname, user_email, user_password, user_image, user_active)
-VALUES(0, 'Facundo', 'Vignoles', 'fvignoles@gmail.com', '$2a$10$EGyDAvj3Dh12qWSGjl886uq2uzmC4gcEPfLBA1Xoisyq3hZDPJ6d.', 'user1637545267021.jpg', 1);
+VALUES(1, 'Facundo', 'Vignoles', 'fvignoles@gmail.com', '$2a$10$EGyDAvj3Dh12qWSGjl886uq2uzmC4gcEPfLBA1Xoisyq3hZDPJ6d.', 'user1637545267021.jpg', 1);
 
 INSERT INTO pizzadb.users
 (user_category_id, user_firstname, user_lastname, user_email, user_password, user_image, user_active)
-VALUES(1, 'Juan', 'P�rez', 'juanperez@gmail.com', '$2a$10$p.BdGLInFuL2RILC2U7m9.fgkm54Q7DgybeWEs2cSefPngX6VPeT.', 'user1637545327683.jpg', 1);
+VALUES(2, 'Juan', 'P�rez', 'juanperez@gmail.com', '$2a$10$p.BdGLInFuL2RILC2U7m9.fgkm54Q7DgybeWEs2cSefPngX6VPeT.', 'user1637545327683.jpg', 1);
 
 INSERT INTO pizzadb.users
 (user_category_id, user_firstname, user_lastname, user_email, user_password, user_image, user_active)
-VALUES(1, 'Jimmy', 'Connors', 'jconnors@gmail.com', '$2a$10$bDL6wsx4C/5yQTxVEAJHb.EQs.qFQ.mW8SU5B7Bxyna2om37t7xnG', '../../public/img/avatardefault.png', 1);
+VALUES(2, 'Jimmy', 'Connors', 'jconnors@gmail.com', '$2a$10$bDL6wsx4C/5yQTxVEAJHb.EQs.qFQ.mW8SU5B7Bxyna2om37t7xnG', '../../public/img/avatardefault.png', 1);
 
 INSERT INTO pizzadb.users
 (user_category_id, user_firstname, user_lastname, user_email, user_password, user_image, user_active)
-VALUES(1, 'Pedro', 'Lista', 'plista@gmail.com', '$2a$10$PM.UvqblMvSAC42YxhPHTeIqyN76Kmc9VFAsc1aekGzs/iSct1Sbm', '../../public/img/avatardefault.png', 1);
+VALUES(2, 'Pedro', 'Lista', 'plista@gmail.com', '$2a$10$PM.UvqblMvSAC42YxhPHTeIqyN76Kmc9VFAsc1aekGzs/iSct1Sbm', '../../public/img/avatardefault.png', 1);
 
 INSERT INTO pizzadb.users
 (user_category_id, user_firstname, user_lastname, user_email, user_password, user_image, user_active)
-VALUES(0, 'as', 'as', 'agustincolman0310@gmail.com', '$2a$10$X8rSdtzOh1zC822QzYtPnOtVI9xiZEiPRfRIQRHZIrbIj.tq1I5vi', 'user1637591965086.png', 1);
+VALUES(1, 'as', 'as', 'agustincolman0310@gmail.com', '$2a$10$X8rSdtzOh1zC822QzYtPnOtVI9xiZEiPRfRIQRHZIrbIj.tq1I5vi', 'user1637591965086.png', 1);
 
------------ Ingredients (pizzas) -----------
+-- --------- Ingredients (pizzas) -----------
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Salsa de Tomate', 1); --id 0
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Salsa de Tomate', 1); -- id 1
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Muzzarella', 1); --id 1
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Muzzarella', 1); -- id 2
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceitunas verdes', 1); --id 2
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceitunas verdes', 1); -- id 3
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceitunas negras', 1); --id 3
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceitunas negras', 1); -- id 4
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso', 1); --id 4
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso', 1); -- id 5
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Tomates en rodajas', 1); --id 5
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Tomates en rodajas', 1); -- id 6
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Or�gano', 1); --id 6
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Oregano', 1); -- id 7
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Cebolla', 1); --id 7
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Cebolla', 1); -- id 8
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Parmesano', 1); --id 8
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Parmesano', 1); -- id 9
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Roquefort', 1); --id 9
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Roquefort', 1); -- id 10
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Provolone', 1); --id 10
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Provolone', 1); -- id 11
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Salame en rodajas', 1); --id 11
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Salame en rodajas', 1); -- id 12
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Fontina', 1); --id 12
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Queso Fontina', 1); -- id 13
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Champignones', 1); --id 13
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Champignones', 1); -- id 14
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Jam�n cocido', 1); --id 14
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Jamon cocido', 1); -- id 15
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Jam�n crudo', 1); --id 15
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Jamon crudo', 1); -- id 16
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Anan� en rodajas', 1); --id 16
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Anana en rodajas', 1); -- id 17
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Huevo', 1); --id 17
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Huevo', 1); -- id 18
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Morrones', 1); --id 18
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Morrones', 1); -- id 19
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Camarones', 1); --id 19
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Camarones', 1); -- id 20
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Tomates Secos', 1); --id 20
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Tomates Secos', 1); -- id 21
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceite de Oliva', 1); --id 21
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Aceite de Oliva', 1); -- id 22
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Albahaca', 1); --id 22
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Albahaca', 1); -- id 23
 
-INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Pollo picado', 1); --id 23
+INSERT INTO pizzadb.ingredients (ingredient_name, ingredient_active) VALUES('Pollo picado', 1); -- id 24
 
------------ sizes (tama�os pizzas) -----------
+-- --------- sizes (tamanos pizzas) -----------
 
-INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Pizzeta', 1); --id 0
+INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Pizzeta', 1); -- id 1
 
-INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Mediana', 1); --id 1
+INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Mediana', 1); -- id 2
 
-INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Grande', 1); --id 2
+INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Grande', 1); -- id 3
 
-INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Gigante', 1); --id 3
+INSERT INTO pizzadb.sizes (size_name, size_active) VALUES('Gigante', 1); -- id 4
 
------------ doughs (masas pizzas) -----------
+-- --------- doughs (masas pizzas) -----------
 
+INSERT INTO pizzadb.doughs (dough_name, dough_active) VALUES('Tradicional', 1); -- id 1
 
+INSERT INTO pizzadb.doughs (dough_name, dough_active) VALUES('Integral', 1); -- id 2
 
-[
- {
-  "id": 1,
-  "name": "Muzzarella",
-  "description": "Salsa de tomates, Muzzarella, Aceitunas verdes o negras a elección",
-  "image": "products/mozzarella.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "600"
- },
- {
-  "id": 2,
-  "name": "Napolitana.",
-  "description": "Queso, tomates en rodajas, ajo, orégano, aceitunas verdes o negras a elección",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "600",
-  "image": "products/napolitana.jpg"
- },
- {
-  "id": 3,
-  "name": "Fugazzeta...",
-  "description": "Queso, Cebolla y Parmesano...",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "600",
-  "image": "products/fugazzeta.jpg"
- },
- {
-  "id": 4,
-  "name": "Roquefort",
-  "description": "Salsa de tomates, queso azul, aceitunas verdes o negras a elección",
-  "image": "products/fugazzeta-al-roquefort.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "700"
- },
- {
-  "id": 5,
-  "name": "Calabresa",
-  "description": "Salsa de tomates, Muzarella, Queso Provolone, Salame en rodajas, Aceitunas verdes o negras a elección",
-  "image": "products/longaniza.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "700"
- },
- {
-  "id": 6,
-  "name": "4 quesos",
-  "description": "Salsa de tomates, Muzarella, Fontina, Roquefort, Provolone",
-  "image": "products/cuatro-quesos.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "800"
- },
- {
-  "id": 7,
-  "name": "Jamón y Champignon",
-  "description": "Salsa de tomates, Queso, Jamón en rodajas, Champignones Aceitunas verdes o negras a elección",
-  "image": "products/jamon-y-champingnon.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "600"
- },
- {
-  "id": 8,
-  "name": "Ananá",
-  "description": "Ananá, Mozarella y Jamón en tiras",
-  "image": "products/anana.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "900"
- },
- {
-  "id": 9,
-  "name": "Camarones",
-  "description": "Huevo, Jamón, Camarones, Morron en tiras (sal y pimienta)",
-  "image": "products/camarones.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "900"
- },
- {
-  "id": 10,
-  "name": "Cuatro Estaciones",
-  "description": "Salsa de tomates, Mozzarella, Jamón, Ananá, Tomates y Queso Parmesano rallado",
-  "image": "products/cuatro-estaciones.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "900"
- },
- {
-  "id": 11,
-  "name": "Rúcula y Jamón Crudo",
-  "description": "Jamón Crudo, Tomates Secos, Mozzarella, Aceite de Oliva y Salsa de tomate",
-  "image": "products/ruculajamoncrudo.png",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "850"
- },
- {
-  "id": 12,
-  "name": "Margarita",
-  "description": "Tomate, Albahaca y Muzzarella",
-  "image": "products/tomates-albahaca.jpg",
-  "size": "Grande",
-  "dough": "Normal/Integral",
-  "price": "700"
- },
- {
-  "id": 13,
-  "name": "De huevo",
-  "description": "Tomate, Albahaca y Muzzarella",
-  "image": "products/huevo.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "700"
- },
- {
-  "id": 14,
-  "name": "Pollo",
-  "description": "Tomate, Pollo y Muzzarella",
-  "image": "products/pollo.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "700"
- },
- {
-  "id": 15,
-  "name": "Fugazzeta al roquefort",
-  "description": "Tomate, Cebolla, Muzzarella y Roquefort",
-  "image": "products/fugazzeta-al-roquefort.jpg",
-  "size": "Grande/Mediana",
-  "dough": "Normal/Integral",
-  "price": "700"
- }
+-- --------- products (pizzas) -----------
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Muzzarella', 'Descripcion Muzzarella', 'products/mozzarella.jpg', 750, 1); -- id 1
+-- Muzzarella, aceitunas verdes
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(1, 1);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Napolitana', 'Descripcion Napolitana', 'products/napolitana.jpg', 650, 1); -- id 2
+-- Queso, tomates en rodajas, ajo, orégano, aceitunas verdes
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(6, 2);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Fugazzeta', 'Descripcion Fugazzeta', 'products/fugazzeta.jpg', 650, 1); -- id 3
+-- Queso, Cebolla y Parmesano
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(8, 3);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Roquefort', 'Descripcion Roquefort', 'products/fugazzeta-al-roquefort.jpg', 700, 1); -- id 4
+-- Queso azul, aceitunas verdes
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(10, 4);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Calabresa', 'Descripcion Calabresa', 'products/longaniza.jpg', 700, 1); -- id 5
+-- Muzarella, Queso Provolone, Salame en rodajas, Aceitunas
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(11, 5);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, '4 quesos', 'Descripcion 4 quesos', 'products/cuatro-quesos.jpg', 800, 1); -- id 6
+-- Muzarella, Fontina, Roquefort, Provolone
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(13, 6);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Jamón y Champignon', 'Descripcion Jamón y Champignon', 'products/jamon-y-champingnon.jpg', 600, 1); -- id 7
+-- Queso, Jamón en rodajas, Champignones Aceitunas verdes
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(14, 7);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Ananá', 'Descripcion Ananá', 'products/anana.jpg', 900, 1); -- id 8
+-- Ananá, Mozarella y Jamón en tiras
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(17, 8);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Camarones', 'Descripcion Camarones', 'products/camarones.jpg', 900, 1); -- id 9
+-- Huevo, Jamón, Camarones, Morron en tiras (sal y pimienta)
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(20, 9);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Cuatro Estaciones', 'Descripcion Cuatro Estaciones', 'products/cuatro-estaciones.jpg', 900, 1); -- id 10
+-- Mozzarella, Jamón, Ananá, Tomates y Queso Parmesano rallado
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(9, 10);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Rúcula y Jamón Crudo', 'Descripcion Rúcula y Jamón Crudo', 'products/ruculajamoncrudo.png', 850, 1); -- id 11
+-- Jamón Crudo, Tomates Secos, Mozzarella, Aceite de Oliva y Salsa de tomate
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(16, 11);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Margarita', 'Descripcion Margarita', 'products/tomates-albahaca.jpg', 700, 1); -- id 12
+-- Tomate, Albahaca y Muzzarella
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(23, 12);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'De huevo', 'Descripcion De huevo', 'products/huevo.jpg', 700, 1); -- id 13
+-- Tomate, Huevo rallado y Muzzarella
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(18, 13);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Pollo', 'Descripcion Pollo', 'products/pollo.jpg', 700, 1); -- id 14
+-- Tomate, Pollo y Muzzarella
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(24, 14);
+
+INSERT INTO pizzadb.products
+(product_size_id, product_dough_id, product_name, product_description, product_image, product_price, product_active)
+VALUES(3, 1, 'Fugazzeta al roquefort', 'Descripcion Fugazzeta al roquefort', 'products/fugazzeta-al-roquefort.jpg', 700, 1); -- id 15
+-- Tomate, Cebolla, Muzzarella y Roquefort
+
+INSERT INTO pizzadb.ingredients_products
+(ingredients_products_ingredientsid, ingredients_products_productid)
+VALUES(10, 15);
+
--- Docs/databases/sql/structure.sql
+++ Docs/databases/sql/structure.sql
@@ -2,26 +2,26 @@ CREATE DATABASE `pizzadb`;
 
 USE `pizzadb`;
 
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
    `user_id` INT NOT NULL AUTO_INCREMENT,
    `user_category_id` INT NOT NULL,
    `user_firstname` CHAR(50) NOT NULL,
    `user_lastname` CHAR(50) NOT NULL,
    `user_email` CHAR(50) NOT NULL,
-   `user_password` CHAR(50) NOT NULL,
+   `user_password` CHAR(100) NOT NULL,
    `user_image` CHAR(50) NOT NULL,
    `user_active` TINYINT NOT NULL,
    PRIMARY KEY (`user_id`)
 );
 
-CREATE TABLE `Categories` (
+CREATE TABLE `categories` (
    `category_id` INT NOT NULL AUTO_INCREMENT,
    `category_name` CHAR(30) NOT NULL,
    `category_active` TINYINT NOT NULL,
    PRIMARY KEY (`category_id`)
 );
 
-CREATE TABLE `Products` (
+CREATE TABLE `products` (
    `product_id` INT NOT NULL AUTO_INCREMENT,
    `product_size_id` INT NOT NULL,
    `product_dough_id` INT NOT NULL,
@@ -33,28 +33,28 @@ CREATE TABLE `Products` (
    PRIMARY KEY (`product_id`)
 );
 
-CREATE TABLE `Sizes` (
+CREATE TABLE `sizes` (
    `size_id` INT NOT NULL AUTO_INCREMENT,
    `size_name` CHAR(30) NOT NULL,
    `size_active` TINYINT NOT NULL,
    PRIMARY KEY (`size_id`)
 );
 
-CREATE TABLE `Doughs` (
+CREATE TABLE `doughs` (
    `dough_id` INT NOT NULL AUTO_INCREMENT,
    `dough_name` CHAR(30) NOT NULL,
    `dough_active` TINYINT NOT NULL,
    PRIMARY KEY (`dough_id`)
 );
 
-CREATE TABLE `Ingredients` (
+CREATE TABLE `ingredients` (
    `ingredient_id` INT NOT NULL AUTO_INCREMENT,
    `ingredient_name` CHAR(30) NOT NULL,
    `ingredient_active` TINYINT NOT NULL,
    PRIMARY KEY (`ingredient_id`)
 );
 
-CREATE TABLE `Carts` (
+CREATE TABLE `carts` (
    `cart_id` INT NOT NULL AUTO_INCREMENT,
    `cart_user_id` INT NOT NULL,
    `cart_quantity` SMALLINT NOT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE `Carts` (
    PRIMARY KEY (`cart_id`)
 );
 
-CREATE TABLE `Products_Carts` (
+CREATE TABLE `products_Carts` (
    `product_cart_id` INT NOT NULL AUTO_INCREMENT,
    `product_cart_cartid` INT NOT NULL,
    `product_cart_productid` INT NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE `Products_Carts` (
    PRIMARY KEY (`product_cart_id`)
 );
 
-CREATE TABLE `Ingredients_Products` (
+CREATE TABLE `ingredients_products` (
    `ingredients_products_id` INT NOT NULL AUTO_INCREMENT,
    `ingredients_products_ingredientsid` INT NOT NULL,
    `ingredients_products_productid` INT NOT NULL,
@@ -79,18 +79,19 @@ CREATE TABLE `Ingredients_Products` (
 );
 
 
-ALTER TABLE `Users` ADD CONSTRAINT `FK_bc7190a9-3c3d-4d28-84ae-663bbe474d0f` FOREIGN KEY (`user_category_id`) REFERENCES `Categories`(`category_id`)  ;
+ALTER TABLE `users` ADD CONSTRAINT `FK_bc7190a9-3c3d-4d28-84ae-663bbe474d0f` FOREIGN KEY (`user_category_id`) REFERENCES `categories`(`category_id`)  ;
 
-ALTER TABLE `Products` ADD CONSTRAINT `FK_1817d770-a15e-4239-8621-c5d3224a10f4` FOREIGN KEY (`product_size_id`) REFERENCES `Sizes`(`size_id`)  ;
+ALTER TABLE `products` ADD CONSTRAINT `FK_1817d770-a15e-4239-8621-c5d3224a10f4` FOREIGN KEY (`product_size_id`) REFERENCES `sizes`(`size_id`)  ;
 
-ALTER TABLE `Products` ADD CONSTRAINT `FK_acc1caa4-cdf1-40ee-b73d-dd64d5663f98` FOREIGN KEY (`product_dough_id`) REFERENCES `Doughs`(`dough_id`)  ;
+ALTER TABLE `products` ADD CONSTRAINT `FK_acc1caa4-cdf1-40ee-b73d-dd64d5663f98` FOREIGN KEY (`product_dough_id`) REFERENCES `doughs`(`dough_id`)  ;
 
-ALTER TABLE `Carts` ADD CONSTRAINT `FK_b47603af-d427-4e5a-aaeb-06ad77116cb8` FOREIGN KEY (`cart_user_id`) REFERENCES `Users`(`user_id`)  ;
+ALTER TABLE `carts` ADD CONSTRAINT `FK_b47603af-d427-4e5a-aaeb-06ad77116cb8` FOREIGN KEY (`cart_user_id`) REFERENCES `users`(`user_id`)  ;
 
-ALTER TABLE `Products_Carts` ADD CONSTRAINT `FK_7891cb92-742d-4b6a-9229-ea3d6490b685` FOREIGN KEY (`product_cart_cartid`) REFERENCES `Carts`(`cart_id`)  ;
+ALTER TABLE `products_carts` ADD CONSTRAINT `FK_7891cb92-742d-4b6a-9229-ea3d6490b685` FOREIGN KEY (`product_cart_cartid`) REFERENCES `carts`(`cart_id`)  ;
 
-ALTER TABLE `Products_Carts` ADD CONSTRAINT `FK_dd7e24d8-0cb5-466c-a60d-3e36740e12da` FOREIGN KEY (`product_cart_productid`) REFERENCES `Products`(`product_id`)  ;
+ALTER TABLE `products_carts` ADD CONSTRAINT `FK_dd7e24d8-0cb5-466c-a60d-3e36740e12da` FOREIGN KEY (`product_cart_productid`) REFERENCES `products`(`product_id`)  ;
 
-ALTER TABLE `Ingredients_Products` ADD CONSTRAINT `FK_00f30552-023b-4ef7-98f9-8dcf46b66460` FOREIGN KEY (`ingredients_products_ingredientsid`) REFERENCES `Ingredients`(`ingredient_id`)  ;
+ALTER TABLE `ingredients_products` ADD CONSTRAINT `FK_00f30552-023b-4ef7-98f9-8dcf46b66460` FOREIGN KEY (`ingredients_products_ingredientsid`) REFERENCES `ingredients`(`ingredient_id`)  ;
 
-ALTER TABLE `Ingredients_Products` ADD CONSTRAINT `FK_032c84e2-16a8-4181-befe-a6a4edcd95e5` FOREIGN KEY (`ingredients_products_productid`) REFERENCES `Products`(`product_id`)  ;
+ALTER TABLE `ingredients_products` ADD CONSTRAINT `FK_032c84e2-16a8-4181-befe-a6a4edcd95e5` FOREIGN KEY (`ingredients_products_productid`) REFERENCES `products`(`product_id`)  ;
+
