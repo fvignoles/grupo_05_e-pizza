189bc61f54c1499b0dd414d1d9bc071e7c51e6be
agustincolman0310 <agustincolman0310@gmail.com>
2022-01-08 22:37:14 -0300

    validacion front

---
+++ public/js/register.js
@@ -0,0 +1,57 @@
+window.addEventListener('load',()=>{
+    let errores = [];
+    const form = document.querySelector("form.login-form");
+
+
+    const firstName = document.querySelector(".firstName");
+    const lastName = document.querySelector(".lastName");
+    const email = document.querySelector(".email");
+    const password = document.querySelector(".password");
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    const minuscula = /[a-z]/;
+    const mayuscula = /[A-Z]/;
+    const numero = /[0-9]/;
+    const caracterEspecial = /[@$.!%#?&]/;
+    form.addEventListener('submit', (e) => {
+    if(firstName.value == ''){
+        errores.push('El campo no puede estar vacío');
+        // firstName.classList.add('error-msg');
+    }else if(firstName.value.length < 2){
+        errores.push('El campo debe tener al menos 2 caracteres');
+        // firstName.classList.add('error-msg');
+    };
+    if(lastName.value == ''){
+        errores.push('El campo no puede estar vacío!!!!!!!!!!!!!!');
+        // firstName.classList.add('error-msg');
+    }else if(lastName.value.length < 2){
+        errores.push('El campo debe tener al menos 2 caracteres!!!!');
+        // firstName.classList.add('error-msg');
+    };
+    if(email.value == ''){
+        errores.push('El mail no puede estar vacio');
+    }else if(!emailRegex.test(email.value)){
+        errores.push('Ingrese un formato de mail válido por favor');
+    };
+    if(password.value == ''){
+        errores.push('La contraseña no puede estar vacía')
+    }else if(password.value.length < 8){
+        errores.push('La contraseña debe tener al menos 8 caracteres')
+    }else{
+        if(!minuscula.test(password.value)) errores.push('La contraseña debe contener al menos una minúscula')
+        if(!mayuscula.test(password.value)) errores.push('La contraseña debe contener al menos una mayúscula')
+        if(!numero.test(password.value)) errores.push('La contraseña debe contener al menos un número')
+        if(!caracterEspecial.test(password.value)) errores.push('La contraseña debe contener al menos un caracter especial')
+    }
+
+
+    if (errores.length > 0) {
+        e.preventDefault();
+        let ulErrors = document.querySelector('.errors');
+        ulErrors.classList.add('error-msg');
+        ulErrors.innerHTML = '';
+        for (let i = 0; i < errores.length; i++) {
+            ulErrors.innerHTML += `<li >  ${errores[i]} </li>`;
+        };
+    }
+}) ;
+})
--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -15,7 +15,10 @@ module.exports = [
     body('password')
         .notEmpty().withMessage('Ingrese una contraseña por favor')
         .isLength({ min: 8}).withMessage('Debe tener al menos 8 caracteres')
-        .matches(/^(?=.\d)(?=.[a-z])(?=.[A-Z])[a-zA-Z\d@$.!%#?&]/).withMessage('Deberá tener letras mayúsculas, minúsculas, un número y un carácter especial.'),
+        .matches(/[a-z]/).withMessage('Debe tener al menor una letra minúscula.')
+        .matches(/[A-Z]/).withMessage('Debe tener al menos una letra mayúscula.')
+        .matches(/[0-9]/).withMessage('Debe tener al menos un número.')
+        .matches(/[@$.!%#?&]/).withMessage('Debe tener al menos un caracter especial(@$.!%#?&).'),
         // .matches(/^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[0-9])[a-zA-Z0-9\d@$.!%#?&]/).withMessage('Deberá tener letras mayúsculas, minúsculas, un número y un carácter especial.'),
     body('image').custom((value, { req }) => {
         let file = req.file;
